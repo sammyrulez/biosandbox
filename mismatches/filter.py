d97de0701988a8067acfe9f2d258990785e27b8a
sammyrulez <sammyrulez@gmail.com>
2012-07-30 23:30:41 +0200

    loading from file and with simple test data

--- mismatches/filter.py
+++ mismatches/filter.py
@@ -1,3 +1,5 @@
+import re
+
 __author__ = 'sam'
 import itertools
 
@@ -18,3 +20,24 @@ def test_mismatch_list():
     out_l = get_mismatch_list("ACGTNAGAAAN", 3)
     for a in out_l:
         print a
+
+def file_mismatch(data_filename,query_filename,mismatch_n):
+
+    data_file = open(data_filename,'r')
+    for r in data_file:
+        #print 'r: ' , r
+        query_file = open(query_filename,'r')
+        for q in query_file:
+            #print 'q: ' , q
+            for c in get_mismatch_list(q,mismatch_n):
+               # print 'c: ' , c
+                if re.search(c, r, re.I):
+                    print q , ' match ' , r , ' with ' , mismatch_n  ,'mismatches as ' , c
+        query_file.close()
+    data_file.close()
+
+def test_file_mismatch():
+    data_txt = 'mismatches/tests/sample_data.txt'
+    queries_txt = 'mismatches/tests/queries.txt'
+    file_mismatch(data_txt, queries_txt,1)
+    file_mismatch(data_txt, queries_txt,3)
